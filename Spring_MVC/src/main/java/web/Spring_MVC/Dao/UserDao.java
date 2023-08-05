fdeb181b6f6ced4fba002c3e39f68581938c4005
bks <andreevbks@gmail.com>
2023-08-05 20:19:17 +0400

    y

--- Spring_MVC/src/main/java/web/Spring_MVC/Dao/UserDao.java
+++ Spring_MVC/src/main/java/web/Spring_MVC/Dao/UserDao.java
@@ -5,13 +5,13 @@ import web.Spring_MVC.Models.User;
 import java.util.List;
 
 public interface UserDao {
-    public List<User> getAllUsers();
+    List<User> getAllUsers();
 
-    public User getUserById(int id);
+    User getUserById(int id);
 
-    public void save(User user);
+    void save(User user);
 
-    public void update(int id, User updateUser);
+    void update(int id, User updateUser);
 
-    public void delete(int id);
+    void delete(int id);
 }
--- Spring_MVC/src/main/java/web/Spring_MVC/Service/UserService.java
+++ Spring_MVC/src/main/java/web/Spring_MVC/Service/UserService.java
@@ -1,16 +1,17 @@
 package web.Spring_MVC.Service;
+
 import web.Spring_MVC.Models.User;
 
 import java.util.List;
 
 public interface UserService {
-    public List<User> getAllUsers();
+    List<User> getAllUsers();
 
-    public User getUserById(int id);
+    User getUserById(int id);
 
-    public void save(User user);
+    void save(User user);
 
-    public void update(int id, User updateUser);
+    void update(int id, User updateUser);
 
-    public void delete(int id);
+    void delete(int id);
 }
